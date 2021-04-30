fd6c6affe802a1ab48ea25a6846492a87907630d
Federico Reina <federicoreinavinchira@yahoo.com>
2021-04-30 01:06:24 -0500

    Initial structure for Main page

---
+++ src/components/Photo/Photo.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Photo(props) {
+
+  return <div></div>
+}
---
+++ src/views/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../../components/Banner/Banner'
+import Photo from '../../components/Photo/Photo'
+
+export default function MainPage(props) {
+  return (
+    <>
+      <Banner/>
+      <Photo/>
+    </>
+  );
+}
