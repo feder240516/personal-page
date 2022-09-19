df6f2d695a46673c3dca9b832bf5438a975a774d
Federico Reina <federicoreinavinchira@gmail.com>
2022-09-19 01:58:44 -0500

    Setup of photo transitions using CSS variables

--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -8,22 +8,22 @@ export default function Photo() {
   return (
     <div className="photo-wrapper">
       <img id="original-photo" src={personalPhoto} alt="Foto de Federico Reina" />
-      <div id="bw-wrapper" className="img-filter-wrapper animated-props">
+      <div id="bw-wrapper" className="img-filter-wrapper animated-props animate-vars">
         <div className="wrapped-wrapper">
           <div id="bw-placeholder" className="img-placeholder animated-props" />
-          <img id="bw-filter" className="img-filter animated-props" src={personalPhoto} alt="Foto de Federico Reina" />
+          <img id="bw-filter" className="img-filter animated-props animate-vars" src={personalPhoto} alt="Foto de Federico Reina" />
         </div>
       </div>
-      <div id="red-wrapper" className="img-filter-wrapper animated-props">
+      <div id="red-wrapper" className="img-filter-wrapper animated-props animate-vars">
         <div className="wrapped-wrapper">
           <div id="red-placeholder" className="img-placeholder animated-props" />
-          <img id="red-filter" className="img-filter animated-props" src={personalPhoto} alt="Foto de Federico Reina" />
+          <img id="red-filter" className="img-filter animated-props animate-vars" src={personalPhoto} alt="Foto de Federico Reina" />
         </div>
       </div>
-      <div id="teal-wrapper" className="img-filter-wrapper animated-props">
+      <div id="teal-wrapper" className="img-filter-wrapper animated-props animate-vars">
         <div className="wrapped-wrapper">
           <div id="teal-placeholder" className="img-placeholder animated-props" />
-          <img id="teal-filter" className="img-filter animated-props" src={personalPhoto} alt="Foto de Federico Reina" />
+          <img id="teal-filter" className="img-filter animated-props animate-vars" src={personalPhoto} alt="Foto de Federico Reina" />
         </div>
       </div>
     </div>
